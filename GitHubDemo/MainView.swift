4505c9426f61e45186b86052839d5bed47d1a515
Sunil <Sunil.prajapati@gmail.com>
2022-01-21 09:40:59 -0500

    Create Second View

--- GitHubDemo/MainView.swift
+++ GitHubDemo/MainView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-// Sunil
+// Sunil Jan 21
 
 struct MainView: View {
     var body: some View {
---
+++ GitHubDemo/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GitHubDemo
+//
+//  Created by Sunil Prajapati on 1/21/22.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
