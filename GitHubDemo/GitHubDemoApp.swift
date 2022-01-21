6e58dd711595b84dcd18f866279783711a483694
Sunil <Sunil.prajapati@gmail.com>
2022-01-21 09:26:46 -0500

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Sunil Prajapati on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
